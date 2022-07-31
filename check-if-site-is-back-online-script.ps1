d8072f0f94311986894a9a5a6949f78a5befd03b
Moses Mwangi <99524902+MosesSoftEng@users.noreply.github.com>
2022-07-31 19:28:55 +0300

    Script to check if site is online

---
+++ check-if-site-is-back-online-script.ps1
@@ -0,0 +1,25 @@
+﻿# Write-Host "Congratulations! Your first script executed successfully"
+
+# Modules imports
+Import-Module BurntToast
+
+$int = 0
+
+while (1){
+	$result = ping john.ur.ac.rw:7771/pls/prodi03/w99pkg.mi_login
+
+	Write-Host $result
+
+	if($result -like "Ping request could not find*") {
+
+		
+	} else {
+		$int = $int + 1
+
+		New-BurntToastNotification -Text 'Check $int: PowerShell notification','Rwanda Application!','Site is online!'
+
+        break
+	}
+
+	Start-Sleep -Seconds 60
+}
